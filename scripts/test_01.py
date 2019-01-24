f5dc312b0d6f1d4fbf1b197d1ab753ddf4677e12
risengzr <46959242+risengzr@users.noreply.github.com>
2019-01-24 18:43:22 +0800

    Update test_01.py

--- scripts/test_01.py
+++ scripts/test_01.py
@@ -3,7 +3,7 @@ import allure
 
 class TestAllure:
 
-    @allure.step("我是测试步骤002")
+    @allure.step("我是测试步骤001")
     def test_01(self):
         allure.attach("描述：", "01")
         assert 1
@@ -11,5 +11,5 @@ class TestAllure:
     @allure.step('我是测试步骤002')
     def test_02(self):
         allure.attach("描述：", "02")
-        assert 0
+        assert 1
 
